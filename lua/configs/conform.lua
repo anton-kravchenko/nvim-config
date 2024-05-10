1166efcb05a95724728cdadc0a8d03c2fdf2bc76
Anton Kravchenko <anton.kravchenko@dish.com>
2024-05-10 15:40:13 -0600

    Fixed auto formatting with prettier

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,15 +3,14 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    typescrupt = { "prettier" },
+    typescript = { "prettier" },
     javascript = { "prettier" },
     markdown = { "prettier" }
   },
 
   format_on_save = {
-    -- These options will be passed to conform.format()
     timeout_ms = 500,
-    lsp_fallback = true,
+    lsp_fallback = true
   },
 }
 
